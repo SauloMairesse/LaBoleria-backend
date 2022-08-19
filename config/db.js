8c282408f5b02e31e8c39a339065bafc504340fc
Saulo <saulodavi2001@gmail.com>
2022-08-19 00:25:42 -0300

    erro deploy

--- config/db.js
+++ config/db.js
@@ -1,15 +1,32 @@
-//SQL
-import pg from "pg"; //SQL
+// //SQL
+// import pg from "pg"; //SQL
+// import dotenv from "dotenv"; //SQL
+// dotenv.config();
+
+// const {Pool} = pg;
+
+// const db = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+
+// });
+
+// export default db;
+import pg from 'pg';
 import dotenv from "dotenv"; //SQL
 dotenv.config();
 
-const {Pool} = pg;
+const { Pool } = pg;
 
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
